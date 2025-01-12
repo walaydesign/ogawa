44cbdf6250a2bd3ee8d231f4c5844e5b89758b93
youzhen <yl301114@gmail.com>
2025-01-12 18:17:28 +0000

    首頁

--- public/js/common.js
+++ public/js/common.js
@@ -1,33 +1,39 @@
 // 深淺模式
 const body = document.body;
-// const toggleIcon = document.getElementById("toggle-icon");
-const toggleText = document.getElementById("toggle-text");
+const toggleText = document.getElementById("mode-toggle-text");
+const textString = ["淺色模式", "深色模式"];
 
-// const iconString = ["light_mode", "dark_mode"];
-const textString = ["Switch to Light Theme", "Switch to Dark Theme"];
-
-// 套用初始文字
 if (window.matchMedia('(prefers-color-scheme: dark)').matches){
-	// toggleIcon.innerText = iconString[0];
 	toggleText.innerText = textString[0];
 } else {
-	// toggleIcon.innerText = iconString[1];
 	toggleText.innerText = textString[1];
 }
 
 function toggleString() {
-    // toggleIcon.innerText = (toggleIcon.innerText === iconString[0]) ? iconString[1] : iconString[0];
     toggleText.innerText = (toggleText.innerText === textString[0]) ? textString[1] : textString[0];
 }
 
+toggleColorMode();
 function toggleColorMode() {
 	if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-		body.classList.toggle("light-mode");
+		body.classList.toggle("dark-mode");
 	} else if(window.matchMedia('(prefers-color-scheme: light)').matches) {
 		body.classList.toggle("dark-mode");
 	}
 	toggleString();
 }
+
+
+// menu
+$(".header_menu").click(function() {
+	$(this).toggleClass("active");
+	$(".header_nav").fadeToggle(300);
+})
+
+$(".header_nav_mode").click(function() {
+	$(".header_menu").removeClass("active");
+	$(".header_nav").fadeOut(300);
+})
 
 
 
