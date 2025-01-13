469ea11f6c1928be143c6266c56de527e401f42d
youzhen <yl301114@gmail.com>
2025-01-13 01:02:56 +0000

    通知

--- public/js/common.js
+++ public/js/common.js
@@ -3,23 +3,30 @@ const body = document.body;
 const toggleText = document.getElementById("mode-toggle-text");
 const textString = ["淺色模式", "深色模式"];
 
-if (window.matchMedia('(prefers-color-scheme: dark)').matches){
-	toggleText.innerText = textString[0];
-} else {
+if (window.matchMedia('(prefers-color-scheme: light)').matches){
+	// body.classList.toggle("dark-mode light-mode");
+	$("body").removeClass("dark-mode").addClass("light-mode");
 	toggleText.innerText = textString[1];
+} else {
+	// body.classList.toggle("light-mode dark-mode");
+	$("body").removeClass("light-mode").addClass("dark-mode");
+	toggleText.innerText = textString[0];
 }
 
 function toggleString() {
     toggleText.innerText = (toggleText.innerText === textString[0]) ? textString[1] : textString[0];
 }
 
-toggleColorMode();
 function toggleColorMode() {
-	if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-		body.classList.toggle("dark-mode");
-	} else if(window.matchMedia('(prefers-color-scheme: light)').matches) {
-		body.classList.toggle("dark-mode");
+	console.log("!!!");
+	if($(body).hasClass("dark-mode")){
+		// body.classList.toggle("light-mode");
+		$("body").removeClass("dark-mode").addClass("light-mode");
+	} else if($(body).hasClass("light-mode")) {
+		// body.classList.toggle("dark-mode");
+		$("body").removeClass("light-mode").addClass("dark-mode");
 	}
+	
 	toggleString();
 }
 
@@ -35,5 +42,31 @@ $(".header_nav_mode").click(function() {
 	$(".header_nav").fadeOut(300);
 })
 
+
+// 返回鍵
+$(".header_back").click(function() {
+	history.back();
+})
+
+
+// 數量選取
+$(".num-box_btn").click(function() {
+	let num = parseInt($(this).parents(".num-box").find(".num-box_num").text());
+	if($(this).hasClass("minus")) {
+		if(num > 0) {
+			num--;
+		}else {
+			num = 0;
+		}
+	}else if($(this).hasClass("plus")) {
+		num++;
+	}
+	$(this).parents(".num-box").find(".num-box_num").text(num);
+})
+
+// popup
+// $(".modal-backdrop").click(function() {
+// 	$(".modal").removeClass("show");
+// })
 
 
